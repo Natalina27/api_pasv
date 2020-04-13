297952e2cf17864df72deeb2f060ec109fc0e87b
Natalya Myunster <natalyamyunster@yahoo.com>
2020-04-13 10:44:47 +0200

    api requests

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+  const getTodos = () =>{
+    console.log('todos is getting execute');
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(json => {
+              console.log(json);
+              setTodos(json);
+        }
+
+        )
+
+  };
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getTodos}> Get todos </button>
+        {
+            todos &&
+                todos.map( el => (
+                    <div key={el.id}>{el.title}</div>
+                ))
+
+        }
     </div>
   );
 }
